635088d797e3200652b72cba2f7ccfb721c9994a
PeaceAndAnarchy <PeaceAndAnarchy@DESKTOP-RKNGVPG>
2019-08-29 17:22:51 +0200

    first

--- src/automation/Automation.java
+++ src/automation/Automation.java
@@ -16,6 +16,8 @@ public class Automation {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello world");
+        //just to test the system out
     }
     
 }
